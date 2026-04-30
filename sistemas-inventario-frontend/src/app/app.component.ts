41e47ac75817bce237191398b436c99ba0e72ec5
SSEBASTHIAN <ssebasthian.perez@gmail.com>
2026-04-30 10:51:44 -0500

    Se Divide Pantalla Menu Vertical y PaginasDinamicas (FRONT- END)

--- sistemas-inventario-frontend/src/app/app.component.ts
+++ sistemas-inventario-frontend/src/app/app.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
 import { MenuComponent } from './pagina/menu/menu.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MenuComponent],
+  imports: [RouterOutlet, MatIconModule, MenuComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'registro-y-iniciosesion-frontend';
+  title = 'sistemas-inventario-frontend';
+
+  menuMinimizado = false; // Estado inicial del menú
+
+
+  menuMinMax() {
+    this.menuMinimizado = !this.menuMinimizado; // Cambia el estado al hacer clic
+  }
 }
